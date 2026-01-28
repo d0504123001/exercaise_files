6d8b2dbfd5e46beec1939492cc679d1ab296836d
david sh <d0504123001@gmail.com>
2026-01-28 16:41:26 +0200

    finish exeraise2,3

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 #תרגיל###1#
 import os
-print(os.getcwd())
+# print(os.getcwd())
 #תרגיל2
+# print(os.listdir())
+#תרדיל3
+# def chdir():
+#     os.chdir(r'C:\Users\esti7\PycharmProjects\io')
+#     print(os.getcwd())
+#     print(os.listdir())
+#     os.chdir(r'C:\Users\esti7\PycharmProjects\exercaise_files')
+#     print(os.getcwd())
+# chdir()
