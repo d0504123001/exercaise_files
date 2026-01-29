2d9be7de8088164b9480bccd28b11c5d0a5c730d
david sh <d0504123001@gmail.com>
2026-01-29 14:32:27 +0200

    finish qustion 5 6

--- main.py
+++ main.py
@@ -11,3 +11,26 @@ import os
 #     os.chdir(r'C:\Users\esti7\PycharmProjects\exercaise_files')
 #     print(os.getcwd())
 # chdir()
+
+#תרגיל5
+# def printed_file():
+#     os.chdir(r'C:\Users\esti7\PycharmProjects')
+#     list_of_files = os.listdir()
+#     print(list_of_files)
+#     for file in list_of_files:
+#         if file .endswith(".py"):
+#             print(list(file))
+# printed_file()
+#תרגיל6
+# def dict_of_files():
+#     dict_files = {'.py':0, '.txt':0, '.pdf':0}
+#     os.chdir(r'C:\Users\esti7\PycharmProjects\exercaise_files')
+#     for file in os.listdir():
+#         if file .endswith(".py"):
+#             dict_files['.py'] +=1
+#         elif file.endswith(".txt"):
+#             dict_files['.txt'] += 1
+#         elif file.endswith(".pdf"):
+#             dict_files['.pdf'] += 1
+#     print(dict_files)
+# dict_of_files()
